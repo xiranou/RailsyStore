e49c00a2b9fb3e68265b7407ad6d2a4fd0552de8
xiranou <xiranou@gmail.com>
2015-03-11 19:12:44 -0400

    generate CartController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'carts/show'
+
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   root 'movies#index'
   resources :movies, only: [:show, :index]
