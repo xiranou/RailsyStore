375b73c23ad3b6d4444c5a7509c2f30b88593c4b
xiranou <xiranou@gmail.com>
2015-03-11 19:04:09 -0400

    update users path name

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   root 'movies#index'
   resources :movies, only: [:show, :index]
 end
