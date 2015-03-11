d818f5cdcfed8d027c1a853fd4cb3a4fde3de38d
xiranou <xiranou@gmail.com>
2015-03-11 19:53:01 -0400

    add cart functions in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,20 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
 
   def cart_count
     $redis.scard("cart#{id}")
+  end
+
+  def cart_total_price
+    total_price = 0
+    get_cart_movies.each { |movie| total_price+= movie.price }
+    total_price
+  end
+
+  def get_cart_movies
+    cart_ids = $redis.smembers "cart#{id}"
+    Movie.find(cart_ids)
   end
 end
