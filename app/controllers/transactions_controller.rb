cb27e766f4b310036a6dcebc0adda2f4d8b68016
xiranou <xiranou@gmail.com>
2015-03-11 20:08:18 -0400

    update TranscationController

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,4 +1,14 @@
 class TransactionsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_cart!
+
   def new
+  end
+
+  private
+  def check_cart!
+    if current_user.get_cart_movies.blank?
+      redirect_to root_url, alert: "Please add some items to your cart before processing your transaction!"
+    end
   end
 end
