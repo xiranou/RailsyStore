c7569fb02f39c9eeb0d10b67f0d52433b58ced9b
xiranou <xiranou@gmail.com>
2015-03-11 20:06:56 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  get 'transactions/new'
+  root 'movies#index'
+
+  resources :movies, only: [:show, :index]
+
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
   resource :cart, only: [:show] do
     put 'add/:movie_id', to: 'carts#add', as: :add_to
     put 'remove/:movie_id', to: 'carts#remove', as: :remove_from
   end
 
-  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  root 'movies#index'
-  resources :movies, only: [:show, :index]
+  resources :transactions, only: [:new, :create]
 end
