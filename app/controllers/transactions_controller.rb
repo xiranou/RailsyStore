de567315a17a22596ec1b535247e440574b86113
xiranou <xiranou@gmail.com>
2015-03-11 20:10:23 -0400

    add braintree generate_client_token to TransactionsController

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,12 +3,18 @@ class TransactionsController < ApplicationController
   before_action :check_cart!
 
   def new
+    gon.client_token = generate_client_token
   end
 
   private
+
   def check_cart!
     if current_user.get_cart_movies.blank?
       redirect_to root_url, alert: "Please add some items to your cart before processing your transaction!"
     end
+  end
+
+  def generate_client_token
+    Braintree::ClientToken.generate
   end
 end
