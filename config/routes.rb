9783dcc16ffcfb053fae4fe10f9a473895e27c38
xiranou <xiranou@gmail.com>
2015-03-11 20:05:59 -0400

    scaffold TransactionsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'transactions/new'
+
   resource :cart, only: [:show] do
     put 'add/:movie_id', to: 'carts#add', as: :add_to
     put 'remove/:movie_id', to: 'carts#remove', as: :remove_from
